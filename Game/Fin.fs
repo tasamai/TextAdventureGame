c97f37bef6efc880df48051842ce3e88ce215f35
IterationStream <samaithomas@gmail.com>
2021-02-09 23:38:18 +0000

    Update Fin.fs

--- Game/Fin.fs
+++ Game/Fin.fs
@@ -6,48 +6,7 @@ open Main
 [<EntryPoint>]
 let main argv =
 
-    //for i in allRooms do
-
-        //while i < 10 do
-
-            //gameWorld
-            ////|> move usermove
-            //|> describeCurrentRoom
-            //|> displayResult
-            //gameWorld
-            //|> move left
-            //|> bind describeCurrentRoom
-            //|> displayResult
-            //|>ignore
-
-    //gameWorld
-    //|> describeCurrentRoom
-    //|> displayResult
-    ////userInput
-    //gameWorld
-    //|> move usermove
-    //|> bind describeCurrentRoom
-    //|> displayResult
-    ////userInput
-    //gameWorld
-    //|> move forward
-    //|> bind describeCurrentRoom
-    //|> displayResult
-    ////userInput
-    //gameWorld
-    //|> move usermove
-    //|> bind describeCurrentRoom
-    //|> displayResult
-
-    //for i in allRooms do
-        //gameWorld
-        ////|> move usermove
-        //|> describeCurrentRoom
-        //|> displayResult
-        //gameWorld
-        //|> (move usermove)
-        //|> bind describeCurrentRoom
-        //|> displayResult
+    
     gameWorld
     |> move usermove
     |> bind describeCurrentRoom
